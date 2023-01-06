6166a9f428e39e87307295211c910d57678fb2d5
areve <areve@challen.info>
2023-01-06 12:53:18 +0000

    mud cat

--- src/views/EditorView.vue
+++ src/views/EditorView.vue
@@ -1,6 +1,13 @@
 <template>
   <main>
-    EditorView
+    <h1>Editor</h1>
+    <form>
+      <label for="prompt">Prompt</label>
+      <input type="text" id="prompt" name="prompt" />
+    </form>
+    <div class="canvas">
+      <img src="/gallery-example/mud-cat.png" />
+    </div>
   </main>
 </template>
 
