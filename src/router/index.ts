7b02f9d70fd799186351ba5e86a4211f59e12493
areve <areve@challen.info>
2023-01-06 14:18:32 +0000

    rename home to gallery

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import GalleryView from "../views/GalleryView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
+      name: "gallery",
+      component: GalleryView,
     },
     {
       path: "/editor",
--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -3,6 +3,6 @@
 
 <template>
   <main>
-    Home
+    <h1>Gallery</h1>
   </main>
 </template>
