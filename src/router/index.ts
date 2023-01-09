f2d2aec22f465dcf57610a61ef7e9933dee7c0a8
areve <areve@challen.info>
2023-01-09 21:00:42 +0000

    editor

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,10 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import GalleryView from "../views/GalleryView.vue";
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     {
       path: "/",
-      name: "gallery",
-      component: GalleryView,
-    },
-    {
-      path: "/editor",
       name: "editor",
       component: () => import("../views/EditorView.vue"),
     },
