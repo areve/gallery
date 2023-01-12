f3fa09f46feedda98ce246b7088db796b8c06188
areve <areve@challen.info>
2023-01-12 16:25:26 +0000

    tidy code

--- api/api.ts
+++ api/api.ts
@@ -3,10 +3,9 @@ import { editorRoutes } from './routes/editor-routes'
 import { openApiProxy } from './routes/openapi-proxy'
 import { galleryRoutes } from './routes/gallery-routes'
 
-const app = express()
 
-app.use('/api/openai/*', openApiProxy);
-app.use('/api/editor', editorRoutes);
-app.use('/api/gallery', galleryRoutes);
 
-export const handler = app
+export const handler = express()
+    .use('/api/openai/*', openApiProxy)
+    .use('/api/editor', editorRoutes)
+    .use('/api/gallery', galleryRoutes)
