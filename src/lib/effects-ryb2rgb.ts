3137a0ce49e13537a8375a6a6684f98512aebeae
areve <areve@challen.info>
2023-02-01 20:03:07 +0000

    thing

--- src/lib/effects-ryb2rgb.ts
+++ src/lib/effects-ryb2rgb.ts
@@ -326,7 +326,7 @@ function ryb2rgbZZ([red, yellow, blue]: [number, number, number]) {
     return [Math.floor(red), Math.floor(green), Math.floor(blue)];
 }
 
-function rgb2rybZZ([red, green, blue]: [number, number, number]) {
+function rgb2rybZZ2([red, green, blue]: [number, number, number]) {
     const white = Math.min(red, green, blue);
 
     red -= white;
@@ -362,3 +362,156 @@ function rgb2rybZZ([red, green, blue]: [number, number, number]) {
 
     return [Math.floor(red), Math.floor(yellow), Math.floor(blue)];
 }
+
+console.log('red', rgb2rybZZ([255, 0, 0]))
+console.log('dark-green', rgb2rybZZ([27, 127, 20]))
+const zzz = rgb2rybZZ([27, 127, 20]) 
+console.log('dark-green', hsv2rgb(zzz))
+
+function rgb2rybZZ([red, green, blue]: [number, number, number]): [number, number, number] {
+    let v = Math.max(red, green, blue)
+    let delta = v - Math.min(red, green, blue)
+
+    let h
+    if (delta === 0) {
+        h = 0
+    } else if (v === red) {
+        h = ((green - blue) / delta + 6) / 6 * 360 % 360
+    } else if (v === green) {
+        h = ((blue - red) / delta + 8) / 6 * 360 % 360
+    } else { // v == blue
+        h = ((red - green) / delta + 10) / 6 * 360 % 360
+    }
+    const s = v && delta / v
+    
+    if (h > 0 && h < 120) {
+        h = h / 2
+        return hsv2rgb([h, s, v ])
+    }
+    if (h >= 120 && h < 180) {
+        h = h - 60
+        return hsv2rgb([h, s, v ])
+    }
+    if (h >= 180 && h < 240) {
+        h = (h - 180) * 2 + 120
+        return hsv2rgb([h, s, v ])
+    }
+    return hsv2rgb([h, s, v ])
+throw ""
+    // http://www.deathbysoftware.com/colors/index.html
+    // red [255, 0, 0] => [255, 0, 0]
+    // dark-green [27,127,20] => [20, 127, 120]
+    //return hsv
+
+    if (true) {
+        let v = Math.max(red, green, blue)
+        let min = Math.min(red, green, blue)
+        let delta = v - min
+
+        if (delta === 0) {
+            return [red, green, blue]
+        } else if (v === red) {
+            //h = ((blue - red) / delta + 8) / 6 % 1
+            return [0, 0, 0]
+        } else if (v === green) {
+            const hue = (green - blue) / delta
+
+            const yellow = 255
+            //const yellowside = (red - green) / delta
+            // const yellow = 
+            //h = ((green - blue) / delta + 6) / 6 % 1
+            //let yellow = Math.min(blue, green);
+            //let r = red -yellow
+
+            //     h = ((green - blue) / delta + 6) / 6 % 1
+            // } else if (v === green) {
+            //     h = ((blue - red) / delta + 8) / 6 % 1
+            // } else { // v == blue
+            //     h = ((red - green) / delta + 10) / 6 % 1
+
+            return [0, 0, 0]
+        } else { // v == blue
+            const hue = (red - green)
+            if (hue >= 0) {
+                return [red, green, blue]
+            }
+            // let r = red -yellow
+
+            return [0, 0, 0]
+        }
+
+    } else {
+        const white = Math.min(red, green, blue);
+        red -= white;
+        green -= white;
+        blue -= white;
+
+        // rgb are now shorter, one is zero
+        const maxBar = Math.max(red, green, blue);
+        let yellow = Math.min(red, green);
+        red -= yellow;
+        green -= yellow;
+        // r & g are now shorter, by yellow
+
+        // only when both green & blue are > 0, i.e. red was shortest
+        if (blue > 0 && green > 0) {
+            blue /= 2;
+            green /= 2;
+        }
+
+        yellow += green;
+        blue += green;
+
+        const maxYellow = Math.max(red, yellow, blue);
+
+        if (maxYellow > 0) {
+            const n = maxBar / maxYellow;
+            red *= n;
+            yellow *= n;
+            blue *= n;
+        }
+
+        red += white;
+        yellow += white;
+        blue += white;
+
+        return [Math.floor(red), Math.floor(yellow), Math.floor(blue)];
+    }
+}
+
+
+//https://stackoverflow.com/questions/2348597/why-doesnt-this-javascript-rgb-to-hsl-code-work/54071699#54071699
+
+type byte = number
+
+// in: r,g,b in [0,1], out: h in [0,360) and s,l in [0,1]
+function rgb2hsl(r: byte, g: byte, b: byte) {
+    let v = Math.max(r, g, b), c = v - Math.min(r, g, b), f = (1 - Math.abs(v + v - c - 1));
+    let h = c && ((v == r) ? (g - b) / c : ((v == g) ? 2 + (b - r) / c : 4 + (r - g) / c));
+    return [60 * (h < 0 ? h + 6 : h), f ? c / f : 0, (v + v - c) / 2];
+}
+
+// input: r,g,b in [0,1], out: h in [0,360) and s,v in [0,1]
+function rgb2hsv(r: byte, g: byte, b: byte) {
+    let v = Math.max(r, g, b), c = v - Math.min(r, g, b);
+    let h = c && ((v == r) ? (g - b) / c : ((v == g) ? 2 + (b - r) / c : 4 + (r - g) / c));
+    return [60 * (h < 0 ? h + 6 : h), v && c / v, v];
+}
+
+function hsv2rgb([h, s, v]: [number, number, number]): [number, number, number]{
+    function fff(n: number, k = (n + h / 60) % 6) {
+        return v - v * s * Math.max(Math.min(k, 4 - k, 1), 0)
+    };
+    return [fff(5), fff(3), fff(1)];
+}
+// // input: h in [0,360] and s,v in [0,1] - output: r,g,b in [0,1]
+// function hsv2rgb(h, s, v) {
+//     let f = (n, k = (n + h / 60) % 6) => v - v * s * Math.max(Math.min(k, 4 - k, 1), 0);
+//     return [f(5), f(3), f(1)];
+// }
+
+
+
+// let hsl2hsv = (h, s, l, v = s * Math.min(l, 1 - l) + l) => [h, v ? 2 - 2 * l / v : 0, v];
+
+// let hsv2hsl = (h, s, v, l = v - v * s / 2, m = Math.min(l, 1 - l)) => [h, m ? (v - l) / m : 0, l];
