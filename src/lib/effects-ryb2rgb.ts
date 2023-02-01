432a770af880f3a5b16d820c944e49a6b0a3c86a
areve <areve@challen.info>
2023-02-01 12:51:18 +0000

    fixed weight

--- src/lib/effects-ryb2rgb.ts
+++ src/lib/effects-ryb2rgb.ts
@@ -57,43 +57,8 @@ export function ryb2rgbEffect(context: CanvasRenderingContext2D) {
 
 
 function cubicInt(t: number, A: number, B: number) {
-    var weight = t * t * (3 - 2 * t);
-    return A + weight * (B - A);
+    return A + t * (B - A);
 }
-
-// function getR(iR: number, iY: number, iB: number) {
-//     // red
-//     var x0 = cubicInt(iB, 1.0, 0.163);
-//     var x1 = cubicInt(iB, 1.0, 0.0);
-//     var x2 = cubicInt(iB, 1.0, 0.5);
-//     var x3 = cubicInt(iB, 1.0, 0.2);
-//     var y0 = cubicInt(iY, x0, x1);
-//     var y1 = cubicInt(iY, x2, x3);
-//     return Math.ceil(255 * cubicInt(iR, y0, y1));
-// }
-
-// function getG(iR: number, iY: number, iB: number) {
-//     // green
-//     var x0 = cubicInt(iB, 1.0, 0.373);
-//     var x1 = cubicInt(iB, 1.0, 0.66);
-//     var x2 = cubicInt(iB, 0.0, 0.0);
-//     var x3 = cubicInt(iB, 0.5, 0.094);
-//     var y0 = cubicInt(iY, x0, x1);
-//     var y1 = cubicInt(iY, x2, x3);
-//     return Math.ceil(255 * cubicInt(iR, y0, y1));
-// }
-
-// function getB(iR: number, iY: number, iB: number) {
-//     // blue
-//     var x0 = cubicInt(iB, 1.0, 0.6);
-//     var x1 = cubicInt(iB, 0.0, 0.2);
-//     var x2 = cubicInt(iB, 0.0, 0.5);
-//     var x3 = cubicInt(iB, 0.0, 0.0);
-//     var y0 = cubicInt(iY, x0, x1);
-//     var y1 = cubicInt(iY, x2, x3);
-//     return Math.ceil(255 * cubicInt(iR, y0, y1));
-// }
-
 
 function ryb2rgb(color: [number, number, number]) {
     var R = color[0] / 255;
