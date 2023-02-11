a8bf3e5e57e8a905a45233a1b9c4245948b91e4e
areve <areve@challen.info>
2023-02-11 17:21:01 +0000

    states all separated

---
+++ src/states/panelsVisibleState.ts
@@ -0,0 +1,15 @@
+import { usePersistentState } from "@/services/persistenceService";
+import { ref } from "vue";
+
+export const panelsVisibleState = ref({
+  settings: false,
+  gallery: true,
+  statusBar: true,
+  openAi: true,
+  artworkSettings: true,
+  scale: true,
+  toolbar: true,
+  pencil: true,
+  menu: true,
+});
+usePersistentState("panelsVisibleState", panelsVisibleState)
