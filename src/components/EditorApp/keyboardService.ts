2dfbff72349a55f09156a589ad0ed6e373de191b
areve <areve@challen.info>
2023-02-12 18:08:35 +0000

    better keycode

--- src/components/EditorApp/keyboardService.ts
+++ src/components/EditorApp/keyboardService.ts
@@ -1,53 +1,19 @@
 import type { MenuItem } from "./MenuItem";
 import { useKeypress } from "vue3-keypress";
 
-const keyCodes: { [key: string]: number } = {
-  "0": 48,
-  "1": 49,
-  "2": 50,
-  "3": 51,
-  "4": 52,
-  "5": 53,
-  "6": 54,
-  "7": 55,
-  "8": 56,
-  "9": 57,
-  a: 65,
-  b: 66,
-  c: 67,
-  d: 68,
-  e: 69,
-  f: 70,
-  g: 71,
-  h: 72,
-  i: 73,
-  j: 74,
-  k: 75,
-  l: 76,
-  m: 77,
-  n: 78,
-  o: 79,
-  p: 80,
-  q: 81,
-  r: 82,
-  s: 83,
-  t: 84,
-  u: 85,
-  v: 86,
-  w: 87,
-  x: 88,
-  y: 89,
-  z: 90,
-  "<": 188,
-  ">": 190,
-  "/": 191,
-};
+import { Key } from "ts-keycode-enum";
+
+function getKeyCode(value: string) {
+  return Key[
+    Object.keys(Key).find((key) => key.toLowerCase() === value.toLowerCase())
+  ];
+}
 
 function keyUpBinds(keyConfigs: any[]) {
   const result: any[] = [];
   keyConfigs.forEach((keyConfig) => {
     result.push({
-      keyCode: keyCodes[keyConfig.key],
+      keyCode: keyConfig.key,
       modifiers: keyConfig.modifiers,
       success: keyConfig.action,
       preventDefault: true,
@@ -61,7 +27,7 @@ function keyDownBinds(keyConfigs: any[]) {
   const result: any[] = [];
   keyConfigs.forEach((keyConfig) => {
     result.push({
-      keyCode: keyCodes[keyConfig.key],
+      keyCode: keyConfig.key,
       modifiers: keyConfig.modifiers,
       success: () => null,
       preventDefault: true,
@@ -73,16 +39,16 @@ function keyDownBinds(keyConfigs: any[]) {
 
 function getModifiers(key: string) {
   const result: string[] = [];
-  if (/Ctrl\+/.test(key)) result.push("ctrlKey");
-  if (/Shift\+/.test(key)) result.push("shiftKey");
+  if (/Ctrl\+/i.test(key)) result.push("ctrlKey");
+  if (/Shift\+/i.test(key)) result.push("shiftKey");
   return result;
 }
 
 function getKey(key: string) {
   let result = key;
-  result = result.replace(/Ctrl\+/, "");
-  result = result.replace(/Shift\+/, "");
-  return result.toLowerCase();
+  result = result.replace(/Ctrl\+/i, "");
+  result = result.replace(/Shift\+/i, "");
+  return getKeyCode(result);
 }
 
 export function useKey(menuItem: MenuItem) {
@@ -94,7 +60,6 @@ export function useKey(menuItem: MenuItem) {
     modifiers,
     action: menuItem.action,
   };
-
   // TODO calling this lots of times is clearly not how it was intededed
   useKeypress({
     keyEvent: "keyup",
@@ -111,7 +76,7 @@ export function useKey(menuItem: MenuItem) {
 export function addKeysForMenuItems(items: MenuItem[]) {
   items.forEach((item) => {
     useKey(item);
-    console.log(item.key);
+
     if (item.items) {
       addKeysForMenuItems(item.items);
     }
