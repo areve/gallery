6d115851ac39180192830657814892def6d788e3
areve <areve@challen.info>
2023-02-21 23:13:37 +0000

    use separate node_modules for api

--- api2/src/index.ts
+++ api2/src/index.ts
@@ -1,4 +1,7 @@
 import express from "express";
 import { helloRoutes } from "./routes/hello-routes";
+import * as ff from '@google-cloud/functions-framework';
 
 export const handler = express().use("/api/hello", helloRoutes);
+
+ff.http('TypescriptFunction', handler);
