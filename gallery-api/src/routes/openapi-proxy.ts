548c7b5058d4f8a9c9121cce1b0c07013eee94f0
areve <areve@challen.info>
2023-02-22 19:26:58 +0000

    stop using dotenv

--- gallery-api/src/routes/openapi-proxy.ts
+++ gallery-api/src/routes/openapi-proxy.ts
@@ -1,8 +1,8 @@
-import dotenv from "dotenv";
+//import dotenv from "dotenv";
 import proxy from "express-http-proxy";
 
-const env = dotenv.config().parsed!;
-const apiKey = env.OPEN_AI_KEY;
+// const env = dotenv.config().parsed!;
+const apiKey = process.env.OPEN_AI_KEY;
 
 export const openApiProxy = proxy("api.openai.com", {
   https: true,
