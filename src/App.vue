6b99372fdbb5262a8f6bcee1798cf32daaaee6af
areve <areve@challen.info>
2023-02-23 19:24:11 +0000

    add google auth

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,67 @@
 <script setup lang="ts">
 import EditorApp from "./components/EditorApp/EditorApp.vue";
+
+// import { onMounted } from "vue";
+// const gAuth = googleAuth.createGAuth({
+//   clientId: "750379347440-mp8am6q8hg41lvkn8pi4jku3eq7ts2lq.apps.googleusercontent.com",
+//   scope: "profile email",
+//   prompt: "select_account",
+//   ux_mode: "redirect",
+//   redirect_uri: document.location.origin,
+// });
+
+// console.log(gAuth)
+
+// onMounted(() => {
+//   let checkGauthLoad = setTimeout(() => {
+//     const isInit = !!gAuth.isInit;
+//     const isAuthorized = !!gAuth.isAuthorized;
+//     const profile = googleUserToProfile(gAuth?.GoogleAuth?.currentUser?.get());
+//     console.log('profile', profile)
+//     if (isInit) {
+//     //  clearInterval(checkGauthLoad);
+//     //   router.replace({ path: "/google-auth" });
+//     }
+//   }, 200);
+// });
+
+// function googleUserToProfile(googleUser: any) {
+//   const profile = googleUser?.getBasicProfile();
+//   console.log('get', profile)
+//  
+//   return (
+//     (profile && {
+//       id: profile.getId(),
+//       name: profile.getName(),
+//       givenName: profile.getGivenName(),
+//       familyName: profile.getFamilyName(),
+//       imageUrl: profile.getImageUrl(),
+//       email: profile.getEmail(),
+//     }) ||
+//     {}
+//   );
+// }
+//     async signIn() {
+//       const googleUser = await gAuth.signIn();
+//       this.isAuthorized = gAuth.isAuthorized;
+//       this.profile = this.googleUserToProfile(googleUser);
+//     },
+//     async signOut() {
+//       await gAuth.signOut();
+//       this.isAuthorized = gAuth.isAuthorized;
+//       this.profile = {};
+//     },
+const callback = (response: any) => {
+  // This callback will be triggered when the user selects or login to
+  // his Google account from the popup
+  console.log("Handle the response", response);
+};
 </script>
 
 <template>
   <div class="app-wrapper">
     <EditorApp />
+    <GoogleLogin :callback="callback" />
   </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from "vue";
-
 import App from "./App.vue";
-
+import vue3GoogleLogin from "vue3-google-login";
 import "./assets/main.css";
 
 const app = createApp(App);
-
+app.use(vue3GoogleLogin, {
+  clientId: "750379347440-mp8am6q8hg41lvkn8pi4jku3eq7ts2lq.apps.googleusercontent.com",
+});
 app.mount("#app");
