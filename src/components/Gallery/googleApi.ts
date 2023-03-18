9717b2657c671f8c9d483078238e544f9359a9b3
areve <areve@challen.info>
2023-03-18 15:45:30 +0000

    sort cache out

--- src/components/Gallery/googleApi.ts
+++ src/components/Gallery/googleApi.ts
@@ -1,6 +1,5 @@
 import { authState } from "@/services/googleAuthService";
-import { cacheFetch, cacheFlush } from "@/services/cacheService";
-import { result } from "lodash";
+import { cacheFetch, cacheFlushKeys } from "@/services/cacheService";
 
 export function escapeQuery(value: string) {
   return value.replace(/\\/g, "\\\\").replace(/'/g, "\\'");
@@ -10,14 +9,33 @@ const fileInfoKeys = ["id", "name", "modifiedTime"];
 interface FileInfo {
   id: string;
   name: string;
-  modifiedTime: string; // TODO is this a string?
+  modifiedTime: string;
 }
 
-// TODO is there a better way in typescipt to handle method signatures?
-function googleUrl(id: string | Record<string, string>, params?: Record<string, string>) {
-  if (typeof id === "object") return `https://www.googleapis.com/drive/v3/files?${new URLSearchParams(id)}`;
+function googleUrl(id: string): string;
+function googleUrl(params: Record<string, string>): string;
+function googleUrl(id: string, params: Record<string, string>): string;
+function googleUrl(idOrParams: string | Record<string, string>, params?: Record<string, string>) {
+  if (typeof idOrParams === "object") {
+    const params = idOrParams;
+    return `https://www.googleapis.com/drive/v3/files?${new URLSearchParams(params)}`;
+  }
+  const id = idOrParams;
   if (params) return `https://www.googleapis.com/drive/v3/files/${id}?${new URLSearchParams(params)}`;
   return `https://www.googleapis.com/drive/v3/files/${id}`;
+}
+
+function googleUploadUrl(id: string): string;
+function googleUploadUrl(params: Record<string, string>): string;
+function googleUploadUrl(id: string, params: Record<string, string>): string;
+function googleUploadUrl(idOrParams: string | Record<string, string>, params?: Record<string, string>) {
+  if (typeof idOrParams === "object") {
+    const params = idOrParams;
+    return `https://www.googleapis.com/upload/drive/v3/files?${new URLSearchParams(params)}`;
+  }
+  const id = idOrParams;
+  if (params) return `https://www.googleapis.com/upload/drive/v3/files/${id}?${new URLSearchParams(params)}`;
+  return `https://www.googleapis.com/upload/drive/v3/files/${id}`;
 }
 
 function getHeaders() {
@@ -28,22 +46,26 @@ async function googleFileBlob(id: string) {
   const url = googleUrl(id, {
     alt: "media",
   });
-  const response = await cacheFetch(url, {
-    method: "GET",
-    headers: getHeaders(),
-  });
+  const response = await cacheFetch(
+    url,
+    {
+      method: "GET",
+      headers: getHeaders(),
+    },
+    `/gallery/${id}`
+  );
   return await response.blob();
 }
 
 async function googleFileUpdate(id: string, name: string, file: Blob) {
-  const metadata = {
-    name,
-    mimeType: "image/png", // TODO this could be automatically found from the blob?
-  };
-  const url = googleUrl(id, {
+  const url = googleUploadUrl(id, {
     uploadType: "multipart",
     fields: fileInfoKeys.join(","),
   });
+  const metadata = {
+    name,
+    mimeType: "image/png",
+  };
   const body = new FormData();
   body.append("metadata", new Blob([JSON.stringify(metadata)], { type: "application/json" }));
   body.append("file", file);
@@ -55,18 +77,16 @@ async function googleFileUpdate(id: string, name: string, file: Blob) {
   return (await response.json()) as FileInfo;
 }
 
-// TODO give make metadata a proper type
 async function googleFileCreate(folderId: string, id: string, name: string, file: Blob) {
-  const metadata = {
-    name,
-    mimeType: "image/png", // TODO this could be automatically found from the blob?
-    parents: [folderId],
-  };
-
-  const url = googleUrl({
+  const url = googleUploadUrl({
     uploadType: "multipart",
     fields: fileInfoKeys.join(","),
   });
+  const metadata = {
+    name,
+    mimeType: "image/png",
+    parents: [folderId],
+  };
   const body = new FormData();
   body.append("metadata", new Blob([JSON.stringify(metadata)], { type: "application/json" }));
   body.append("file", file);
@@ -79,15 +99,13 @@ async function googleFileCreate(folderId: string, id: string, name: string, file
 }
 
 async function googleFolderCreate(name: string) {
-  const params: Record<string, string> = {
+  const url = googleUrl({
     fields: fileInfoKeys.join(","),
-  };
+  });
   const body = {
     name,
     mimeType: "application/vnd.google-apps.folder",
   };
-
-  const url = googleUrl(params);
   const response = await fetch(url, {
     method: "POST",
     headers: getHeaders(),
@@ -100,10 +118,14 @@ async function googleFileGet(id: string) {
   const url = googleUrl(id, {
     fields: fileInfoKeys.join(","),
   });
-  const response = await cacheFetch(url, {
-    method: "GET",
-    headers: getHeaders(),
-  });
+  const response = await cacheFetch(
+    url,
+    {
+      method: "GET",
+      headers: getHeaders(),
+    },
+    `/gallery/${id}/metadata`
+  );
   return (await response.json()) as FileInfo;
 }
 
@@ -116,7 +138,7 @@ async function googleFileDelete(id: string) {
   return response.status === 204;
 }
 
-async function googleFilesGet(params: Record<string, string>, flush?: boolean) {
+async function googleFilesGet(params: Record<string, string>, cacheKey: string) {
   const url = googleUrl(params);
   const response = await cacheFetch(
     url,
@@ -124,19 +146,21 @@ async function googleFilesGet(params: Record<string, string>, flush?: boolean) {
       method: "GET",
       headers: getHeaders(),
     },
-    flush
+    cacheKey
   );
   const result = await response.json();
-  // TODO there is nextPageToken and files, as requested
   return result.files as FileInfo[];
 }
 
 export async function folderExists(name: string) {
-  const result = await googleFilesGet({
-    q: `trashed=false and name='${escapeQuery(name)}' and mimeType='application/vnd.google-apps.folder'`,
-    pageSize: "1",
-    fields: `files(${fileInfoKeys.join(",")})`,
-  });
+  const result = await googleFilesGet(
+    {
+      q: `trashed=false and name='${escapeQuery(name)}' and mimeType='application/vnd.google-apps.folder'`,
+      pageSize: "1",
+      fields: `files(${fileInfoKeys.join(",")})`,
+    },
+    "/"
+  );
   return result[0];
 }
 
@@ -158,7 +182,7 @@ export async function listFiles() {
       pageSize: "12", // TODO set to something big?
       fields: `nextPageToken, files(${fileInfoKeys.join(",")})`,
     },
-    true
+    "/gallery"
   );
 }
 
@@ -171,13 +195,16 @@ export async function getFile(id: string) {
 }
 
 export async function deleteFile(id: string) {
-  await cacheFlush(); // TODO a bit of overkill to flush the entire cache
+  await cacheFlushKeys([`/gallery/${id}`, `/gallery/${id}/metadata`, "/gallery"]);
   return googleFileDelete(id);
 }
 
 export async function saveFile(id: string, name: string, file: Blob) {
-  await cacheFlush(); // TODO a bit of overkill to flush the entire cache
   const folder = await ensureFolder("gallery.challen.info"); // TODO hard coded folder name?
-  if (id) return await googleFileUpdate(id, name, file);
+  if (id) {
+    await cacheFlushKeys([`/gallery/${id}`, `/gallery/${id}/metadata`]);
+    return await googleFileUpdate(id, name, file);
+  }
+  await cacheFlushKeys([`/gallery`]);
   return await googleFileCreate(folder.id, id, name, file);
 }
