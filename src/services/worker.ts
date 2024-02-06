65ca553f563fbd017eac2641cb16e8027dc902a2
areve <areve@challen.info>
2024-02-06 20:39:43 +0000

    blue again

--- src/services/worker.ts
+++ src/services/worker.ts
@@ -55,27 +55,28 @@ let i = 0;
 let start = new Date().getTime();
 
 function animate() {
-  //   if (context) { // was it this?
-  i++;
+  if (context) {
+    // was it this?
+    i++;
 
-  if (!imageData) imageData = context.getImageData(0, 0, context.canvas.width, context.canvas.height);
-  const data = imageData.data;
+    if (!imageData) imageData = context.getImageData(0, 0, context.canvas.width, context.canvas.height);
+    const data = imageData.data;
 
-  for (let i = 0; i < data.length; i++) {
-    data[i] = Math.random() * 255;
+    for (let i = 0; i < data.length; i++) {
+      data[i] = Math.random() * 255;
+    }
+
+    context.putImageData(imageData, 0, 0);
+
+    const frameInterval = 100;
+    if (i % frameInterval === 0) {
+      const end = new Date().getTime();
+      const duration = end - start;
+      const fps = Math.round((frameInterval / duration) * 1000);
+      console.log(`${frameInterval} frames took ${duration}ms, ${fps}fps`);
+      start = new Date().getTime();
+    }
   }
-
-  context.putImageData(imageData, 0, 0);
-
-  const frameInterval = 100;
-  if (i % frameInterval === 0) {
-    const end = new Date().getTime();
-    const duration = end - start;
-    const fps = Math.round((frameInterval / duration) * 1000);
-    console.log(`${frameInterval} frames took ${duration}ms, ${fps}fps`);
-    start = new Date().getTime();
-  }
-
   requestAnimationFrame(animate);
 }
 
