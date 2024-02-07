2ddaae10a0346c5b5b068d5b04a853ae43d469a6
areve <areve@challen.info>
2024-02-07 17:34:00 +0000

    slimmed worker

--- src/workers/artboardWorker.ts
+++ src/workers/artboardWorker.ts
@@ -5,47 +5,33 @@ import type { Brush } from "@/interfaces/Brush";
 import { createBitmapLayer } from "@/lib/bitmap-layer";
 import { applyBrush, createBrush } from "@/lib/bitmap/bitmap-brush";
 import { resetAll } from "@/lib/bitmap/bitmap-effects";
-import { color2srgb, colorConverter } from "@/lib/color/color";
+import { colorConverter } from "@/lib/color/color";
 import { rectsOverlappedByAny } from "@/lib/rect";
 import { ref, watchPostEffect } from "vue";
 
 let context: OffscreenCanvasRenderingContext2D | null = null;
 let canvas: OffscreenCanvas;
-let imageData: ImageData | null = null;
+
 let tempImageData: ImageData | null = null;
 let i = 0;
 let start = new Date().getTime();
 let bitmapLayer: BitmapLayer | null = null;
-let colorSpace = ref<ColorSpace>("oklch");
+const colorSpace = ref<ColorSpace>("oklch");
 let brush: Brush | undefined = undefined;
-function animate() {
-  if (context) {
-    i++;
 
-    if (!imageData) imageData = context.getImageData(0, 0, context.canvas.width, context.canvas.height);
-    const data = imageData.data;
-
-    for (let i = 0; i < data.length; i++) {
-      data[i] = Math.random() * 255;
-    }
-
-    context.putImageData(imageData, 0, 0);
-
-    const frameInterval = 100;
-    if (i % frameInterval === 0) {
-      const end = new Date().getTime();
-      const duration = end - start;
-      const fps = Math.round((frameInterval / duration) * 1000);
-      console.log(`${frameInterval} frames took ${duration}ms, ${fps}fps`);
-      start = new Date().getTime();
-    }
+function frameCounter() {
+  const calculateAfterFrames = 60;
+  if (++i % calculateAfterFrames === 0) {
+    const end = new Date().getTime();
+    const duration = end - start;
+    const fps = Math.round((calculateAfterFrames / duration) * 1000);
+    console.log(`${fps}fps`);
+    start = new Date().getTime();
   }
-  //  requestAnimationFrame(animate);
 }
 
 function reset() {
   if (!context) return;
-  // const context = context;
   const height = context.canvas.height;
   const width = context.canvas.width;
   bitmapLayer = createBitmapLayer(width, height, colorSpace.value, 32);
@@ -55,13 +41,10 @@ function reset() {
 function render() {
   if (!context) return;
   if (!bitmapLayer) return;
-  // if (!bitmapLayer.dirty.length) return;
-
+  frameCounter();
   const dirtyTiles = rectsOverlappedByAny(bitmapLayer.tiles, bitmapLayer.dirty);
   dirtyTiles.forEach(renderRect);
   bitmapLayer.dirty = [];
-
-  // animate();
   requestAnimationFrame(render);
 }
 
@@ -73,7 +56,6 @@ function renderRect(rect: Rect) {
     tempImageData = new ImageData(rect.width, rect.height);
   }
 
-  // const context = context;
   const pixelData = tempImageData.data;
   const layerData = bitmapLayer.data;
   const width = bitmapLayer.width;
@@ -100,7 +82,6 @@ watchPostEffect(() => {
 
 onmessage = function (event: MessageEvent) {
   colorSpace.value = event.data.colorSpace || colorSpace.value;
-  console.log("applyBrush", !!brush, event.data.params);
   if (event.data.canvas) {
     canvas = event.data.canvas;
     context = canvas.getContext("2d");
@@ -119,15 +100,12 @@ onmessage = function (event: MessageEvent) {
     }
     if (event.data.action === "applyBrush") {
       if (!brush) brush = createColoredBrush(event.data.params.colorSpace, event.data.params.color, event.data.params.radius);
-      // console.log("applyBrush",  event.data.params);
-      applyBrush(bitmapLayer, event.data.params.brushLastPoint, event.data.params.canvasPoint, brush, event.data.params.weight)
+      applyBrush(bitmapLayer, event.data.params.brushLastPoint, event.data.params.canvasPoint, brush, event.data.params.weight);
     }
-    // resetAll(bitmapLayer, event.data.resetAll);
   }
 };
 
 function createColoredBrush(colorSpace: ColorSpace, color: ColorCoord, radius: number) {
-  //const color = colorSpace;
   return createBrush(radius, color, colorSpace);
 }
 
