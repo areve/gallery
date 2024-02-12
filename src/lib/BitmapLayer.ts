a54b8c229f8340b1c061b5d64babe46723040d83
areve <areve@challen.info>
2024-02-12 13:55:43 +0000

    move rect

--- src/lib/BitmapLayer.ts
+++ src/lib/BitmapLayer.ts
@@ -1,4 +1,4 @@
-import { createTiles, rectsOverlappedByAny, type Rect } from "@/lib/Rect";
+import { createTiles, rectsOverlappedByAny, type Rect } from "@/lib/_Rect";
 import { colorConverter } from "./color/color";
 
 export type ColorSpace = "oklch" | "srgb";
