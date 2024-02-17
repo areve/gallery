af7e3f184abd8a5cab3d9b36209c5fd33f592dc2
areve <areve@challen.info>
2024-02-17 10:03:35 +0000

    change permissiosn

--- src/lib/Google/googleAuthConfig.ts
+++ src/lib/Google/googleAuthConfig.ts
@@ -1,7 +1,6 @@
 export const googleAuthConfig = {
   clientId: "750379347440-mp8am6q8hg41lvkn8pi4jku3eq7ts2lq.apps.googleusercontent.com",
   idScope: "email profile openid",
-  // TODO change to use this instead, it's got way less access
-  // accessScope: " https://www.googleapis.com/auth/drive.file",
-  accessScope: "https://www.googleapis.com/auth/drive",
+  // TODO change to use "drive.appfolder" or give options for "drive"
+  accessScope: " https://www.googleapis.com/auth/drive.file",
 };
