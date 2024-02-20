3c06a58fc2d41930b9d07e60a7f1cc21da832c0e
areve <areve@challen.info>
2024-02-20 19:21:32 +0000

    tidy

--- src/components/Gallery/GalleryWorker.ts
+++ src/components/Gallery/GalleryWorker.ts
@@ -1,7 +1,7 @@
 import { googleFileUpdate, googleFileCreate, googleFileBlob, googlePathGetOrCreate, googleFileGet, googlePathGet } from "@/lib/Google/GoogleApi";
 import { createMessageBus } from "@/lib/MessageBus";
-import { notifyError as progressError, notifyProgress as progressMessage, progressState, type ProgressState } from "../Progress/progressState";
-import { ref, watchPostEffect } from "vue";
+import { notifyError as progressError, notifyProgress as progressMessage, progressState } from "../Progress/progressState";
+import { watchPostEffect } from "vue";
 import { clone } from "@/lib/utils";
 import type { Artwork, ArtworkWithBlob } from "./Artwork";
 
