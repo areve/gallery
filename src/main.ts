e6c41214263cd9b2c972225d0ef575a18aa993ff
areve <areve@challen.info>
2024-02-21 22:51:42 +0000

    onNeedRefresh error

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,18 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/main.css";
 import { registerSW } from "virtual:pwa-register";
+import { progressError } from "./components/Progress/progressState";
 
 registerSW({
   immediate: true,
   onNeedRefresh() {
     console.log("onNeedRefresh");
+    // TODO see https://vite-pwa-org.netlify.app/frameworks/
+    progressError("onNeedRefresh does not work yet");
   },
   onOfflineReady() {
     console.log("onOfflineReady");
+    progressError("onOfflineReady does not work yet");
   },
   onRegisteredSW() {
     console.log("onRegisteredSW");
