63a513ee3d1a6d532dc60fb97d66ed839eeb109b
areve <areve@challen.info>
2024-02-21 22:46:45 +0000

    VitePWA prompt

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(({ command }) => {
       },
     }),
     VitePWA({
-      registerType: "autoUpdate",
+      registerType: "prompt",
       includeAssets: ["/icons/favicon.ico", "/icons/apple-touch-icon.png"],
       devOptions: {
         enabled: true,
