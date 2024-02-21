4d72aa36e945c180b1326e65fab0796113231661
areve <areve@challen.info>
2024-02-21 22:45:27 +0000

    edge button can move

--- src/components/EdgeButton/EdgeButton.vue
+++ src/components/EdgeButton/EdgeButton.vue
@@ -1,11 +1,39 @@
 <template>
-  <button class="edge-button" type="button" @click="nothing"></button>
+  <button
+    class="edge-button"
+    type="button"
+    @click="nothing"
+    ref="button"
+    :style="{
+      top: topPercent + 'px',
+    }"
+  ></button>
 </template>
 
 <script lang="ts" setup>
+import { ref, watchSyncEffect } from "vue";
 import { progressMessage } from "../Progress/progressState";
+import { gestureAnyEvent } from "@/lib/GestureEvent";
 
+const button = ref<HTMLCanvasElement>(undefined!);
+const topPercent = ref<number>(100);
+// let cancelNextPress = false;
+watchSyncEffect(() => {
+  if (!gestureAnyEvent.value) return;
+  if (gestureAnyEvent.value.firstEvent.target !== button.value) return;
+  const yDiff = gestureAnyEvent.value.firstEvent.screen.y - gestureAnyEvent.value.currentEvent.screen.y;
+  const yDiff2 = gestureAnyEvent.value.previousEvent ? gestureAnyEvent.value.currentEvent.screen.y - gestureAnyEvent.value.previousEvent.screen.y : 0;
+  if (Math.abs(yDiff) > 10) {
+    // cancelNextPress = true;
+    topPercent.value += yDiff2;
+    console.log(yDiff, yDiff2);
+  }
+});
 function nothing() {
+  // if (cancelNextPress) {
+    // cancelNextPress = false;
+    // return;
+  // }
   progressMessage("button does nothing yet", 2);
   setTimeout(() => progressMessage("done"), 2000);
 }
