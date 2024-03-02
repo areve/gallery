248ef6f698c09ccb334e5225ca3f7dfe9c03524a
areve <areve@challen.info>
2024-03-02 22:19:36 +0000

    debug

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/main.css";
 
-if (typeof window !== "undefined") import("./update");
+// if (typeof window !== "undefined") import("./update");
 // import { registerSW } from "virtual:pwa-register";
 // import { notifyError, notifyToast } from "./components/Notify/notifyState";
 // import { cloneExtend } from "./lib/utils";
