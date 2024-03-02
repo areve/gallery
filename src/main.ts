73bf555dede4555265124a2935092a719df9648b
areve <areve@challen.info>
2024-03-02 12:43:42 +0000

    debug #8

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ const updateSW = registerSW({
       setInterval(() => {
         appState.value.checkCount++;
         registration.update();
-        notifyToast("check for update #7");
+        notifyToast("check for update #8");
       }, 15000);
   },
   onRegisterError(error: any) {
