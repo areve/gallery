a989f183a1a549ec922e80bf77a0b3eb54140381
areve <areve@challen.info>
2024-03-03 09:00:28 +0000

    #31

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,46 @@
 <script setup lang="ts">
+import { appState } from "./appState";
+import { watch } from "vue";
+import { registerSW } from "virtual:pwa-register";
+import { notifyError, notifyToast } from "./components/Notify/notifyState";
+import { cloneExtend } from "./lib/utils";
 import ArtApp from "@/components/ArtApp/ArtApp.vue";
-import ReloadPrompt from "@/components/ReloadPrompt/ReloadPrompt.vue";
+
+const updateNow = () => {
+  notifyToast("update now");
+  if (!appState.value.updateApproved) return;
+  notifyToast("update now and approved");
+  appState.value = cloneExtend(appState.value, {
+    updateApproved: false,
+    updateAvailable: false,
+  });
+  updateSW(true);
+};
+
+watch(() => appState.value.updateApproved, updateNow);
+
+const updateSW = registerSW({
+  immediate: false,
+  onNeedRefresh() {
+    notifyToast("update available");
+    appState.value.updateAvailable = true;
+  },
+  onOfflineReady() {
+    notifyToast("on offline ready");
+  },
+  onRegisteredSW(swScriptUrl: string, registration: ServiceWorkerRegistration | undefined) {
+    notifyToast(swScriptUrl);
+    registration &&
+      setInterval(() => {
+        appState.value.checkCount++;
+        notifyToast("check for update #31");
+        registration.update();
+      }, 15000);
+  },
+  onRegisterError(error: any) {
+    notifyError("onRegisterError " + JSON.stringify(error));
+  },
+});
 </script>
 
 <template>
--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,6 @@
-// import { appState } from "./appState";
-// import { createApp, watch } from "vue";
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/main.css";
-
-// if (typeof window !== "undefined") import("./update");
-// import { registerSW } from "virtual:pwa-register";
-// import { notifyError, notifyToast } from "./components/Notify/notifyState";
-// import { cloneExtend } from "./lib/utils";
-
-// const updateNow = () => {
-//   notifyToast("update now");
-//   if (!appState.value.updateApproved) return;
-//   notifyToast("update now and approved");
-//   appState.value = cloneExtend(appState.value, {
-//     updateApproved: false,
-//     updateAvailable: false,
-//   });
-//   updateSW(true);
-// };
-
-// watch(() => appState.value.updateApproved, updateNow);
-
-// const updateSW = registerSW({
-//   immediate: false,
-//   onNeedRefresh() {
-//     notifyToast("update available");
-//     appState.value.updateAvailable = true;
-//   },
-//   onOfflineReady() {
-//     notifyToast("on offline ready");
-//   },
-//   onRegisteredSW(swScriptUrl: string, registration: ServiceWorkerRegistration | undefined) {
-//     notifyToast(swScriptUrl);
-//     registration &&
-//       setInterval(() => {
-//         appState.value.checkCount++;
-//         notifyToast("check for update #11");
-//         registration.update();
-//       }, 15000);
-//   },
-//   onRegisterError(error: any) {
-//     notifyError("onRegisterError " + JSON.stringify(error));
-//   },
-// });
 
 const app = createApp(App);
 app.mount("#app");
